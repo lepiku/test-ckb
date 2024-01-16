cb7ebe9dcb541ce3a4ee2096089c99d810bd0e0c
Muhammad Oktoluqman Fakhrianto <dimas.oktoluqman@gmail.com>
2024-01-16 07:45:25 +0700

    chore: readme with images

---
+++ README.md
@@ -0,0 +1,7 @@
+## Page 1
+
+![page 1](./images/page1.png)
+
+## Page 2
+
+![page 2](./images/page2.png)
--- test-web-apps/src/views/TwoView.vue
+++ test-web-apps/src/views/TwoView.vue
@@ -1,67 +1,8 @@
 <script setup>
-import { reactive } from 'vue'
-import { BCard, BForm, BButton, BFormInput, BFormGroup } from 'bootstrap-vue'
+import { BCard } from 'bootstrap-vue'
 import MahasiswaCard from '@/components/MahasiswaCard.vue'
 import MataKuliahCard from '@/components/MataKuliahCard.vue'
 import NilaiMahasiswaCard from '@/components/NilaiMahasiswaCard.vue'
-const formMahasiswa = reactive({
-  nim: '',
-  nama_mahasiswa: '',
-  jurusan: ''
-})
-const formMataKuliah = reactive({
-  kode_mata_kuliah: '',
-  nama_mata_kuliah: '',
-  sks: 0
-})
-const formNilaiMahasiswa = reactive({
-  nim: '',
-  kode_mata_kuliah: '',
-  tugas: 0,
-  uas: 0
-})
-const form = {
-  mahasiswa: {
-    title: 'Mahasiswa',
-    model: formMahasiswa,
-    fields: [
-      { name: 'nim', maxlength: 50 },
-      { name: 'nama_mahasiswa', maxlength: 50 },
-      { name: 'jurusan', maxlength: 50 }
-    ],
-    onSubmit(event) {
-      event.preventDefault()
-      alert(JSON.stringify(formMahasiswa))
-    }
-  },
-  mataKuliah: {
-    title: 'Mata Kuliah',
-    model: formMataKuliah,
-    fields: [
-      { name: 'kode_mata_kuliah', maxlength: 50 },
-      { name: 'nama_mata_kuliah', maxlength: 50 },
-      { name: 'sks', type: 'number' }
-    ],
-    onSubmit(event) {
-      event.preventDefault()
-      alert(JSON.stringify(formMataKuliah))
-    }
-  },
-  nilaiMahasiswa: {
-    title: 'Nilai Mahasiswa',
-    model: formNilaiMahasiswa,
-    fields: [
-      { name: 'nim', maxlength: 50 },
-      { name: 'kode_mata_kuliah', maxlength: 50 },
-      { name: 'tugas', type: 'number' },
-      { name: 'uas', type: 'number' }
-    ],
-    onSubmit(event) {
-      event.preventDefault()
-      alert(JSON.stringify(formNilaiMahasiswa))
-    }
-  }
-}
 </script>
 
 <template>
